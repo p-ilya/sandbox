21c848daa01389d2d2eb7895722a4f6039b94152
Machine Owner <owner@debian.lan>
2016-09-13 15:04:59 -0500

    Reply markup working

--- db_utils.py
+++ db_utils.py
@@ -18,11 +18,11 @@ def get_faculty_names():
         p = cur.fetchall()
         return p
         
-def get_groups_by_f(f):
+def get_groups_by_f(f,y):
     with con:
         con.row_factory = sq.Row
         cur = con.cursor()
-        cur.execute("SELECT g_id,name FROM groups WHERE f_id=?",(f,))
+        cur.execute("SELECT g_id,name FROM groups WHERE f_id=? AND year=?",(f,y))
         p = cur.fetchall()
         return p
 
@@ -33,15 +33,42 @@ def get_faculty_name(f_id):
         f = cur.fetchone()
         return f[0]
         
-def get_sch_by_date(date):
+def get_sch_by_date(user,date):
     ts = []    
     with con:
         con.row_factory = sq.Row
         cur = con.cursor()
-        cur.execute('SELECT * FROM classes WHERE c_date=?',(date,))
+        cur.execute("SELECT g_id FROM users WHERE u_id=?",user)
+        u = cur.fetchone()
+        cur.execute('SELECT * FROM classes WHERE c_date=? AND g_id=?',(date,u[0]))
         p = cur.fetchall()
         for row in p:
             msg = '{0}, {1} пара (неделя {2}):\n{3}\n{4}\n{5}\n'.format(row['c_date'], row['c_time'],row['week'],row['name'],row['prepod'],row['place'])
             ts.append(msg)
     return ts
-            
+    
+def add_new_user(u_id,g_id):
+    with con:
+        con.row_factory = sq.Row
+        cur = con.cursor()
+        cur.execute("INSERT INTO users VALUES(?,?)",(u_id,g_id))
+    return True
+    
+def delete_user(u_id):
+    with con:
+        con.row_factory = sq.Row
+        cur = con.cursor()
+        cur.execute("DELETE FROM users WHERE u_id=?",(u_id,))
+    return True
+    
+def check_users():
+    tc = []
+    with con:
+        con.row_factory = sq.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM users")
+        p = cur.fetchall()
+        for row in p:
+            z = 'User ID: {0}\nGroup ID: {1}\n\n'.format(row['u_id'],row['g_id'])
+            tc.append(z)
+    return tc
--- uneconbot_co.py
+++ uneconbot_co.py
@@ -5,40 +5,95 @@ Created on Wed Sep  7 06:38:54 2016
 @author: owner
 """
 
-import sqlite3 as sq
-import telebot, time
+import telebot
 import logging
+import db_utils
 
-con = sq.connect('unecon_bot.db', check_same_thread=False)
-
-token = 'XXXXXXXXX:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(db_utils.bot_config.TOKEN)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
-welcome_msg = '''Бот предназначен для помощи с расписанием пар студентам СПбГЭУ.'''
+welcome_msg = '''Бот предназначен для помощи с расписанием пар студентам СПбГЭУ.\n
+Пока что напрочь недоделан, поэтому оставь попытки взаимодействия всяк сюда входящий :)'''
 
 @bot.message_handler(commands=['start','help'])
 def start_help(message):
     bot.send_message(message.chat.id, welcome_msg)
+
+users_to_register = {}
+  
+@bot.message_handler(commands=['new'])
+def register(message):
+    user_id = message.from_user.id
+    users_to_register[user_id] = [0,0,0] #fak,god,group
+    f_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for row in db_utils.get_faculty_names(): 
+        button = '{1} {0}'.format(row['name'], row['f_id'])     
+        f_markup.add(button)            
+    msg = bot.send_message(message.chat.id,'Я запомнил ваш id.\nВыберите ваш факультет, пожалуйста.',reply_markup=f_markup)
+    bot.register_next_step_handler(msg,send_year_list)
+
+def send_year_list(message):
+    answer = message.text.split(' ',1)
+    flty = int(answer[0])
+    users_to_register[message.from_user.id][0]=flty
+    y_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for i in range(1,7): 
+        y_markup.add('{} год обучения'.format(i))
+    msg = bot.send_message(message.chat.id,'Выберите курс:',reply_markup=y_markup)
+    bot.register_next_step_handler(msg,send_group_list)
+
+def send_group_list(message):
+    answer = message.text.split(' ',1)
+    year = int(answer[0])
+    users_to_register[message.from_user.id][1]=year
+    flty = users_to_register[message.from_user.id][0]
+    g_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for row in db_utils.get_groups_by_f(flty,year): 
+        button = '{1} {0}'.format(row['name'],row['g_id'])        
+        g_markup.add(button)
+    chosen_f = db_utils.get_faculty_name(flty)
+    msg = bot.send_message(message.from_user.id,'Вы выбрали {}.\nВыберите вашу группу:'.format(chosen_f),reply_markup=g_markup)
+    bot.register_next_step_handler(msg,confirm_changes)
     
-@bot.message_handler(commands=['test'])
+def confirm_changes(message):
+    answer = message.text.split(' ',1)
+    group = int(answer[0])
+    users_to_register[message.from_user.id][2]=group
+    r = db_utils.add_new_user(message.chat.id,users_to_register[message.chat.id][2])
+    if not r: 
+        bot.send_message(message.chat.id,"Что-то сломалось. Попробуйте настроить снова.")
+    else:
+        bot.send_message(message.chat.id,"Я вас запомнил, приятно познакомиться!")
+    
+#@bot.callback_query_handler(func=lambda call: True)
+'''def send_group_list(call):
+    answer = call.data.split(' ')
+    flty = int(answer[1])
+    g_markup = telebot.types.InlineKeyboardMarkup()
+    for row in db_utils.get_groups_by_f(flty): 
+        button = telebot.types.InlineKeyboardButton(text=row['name'], callback_data='g_id {}'.format(row['g_id']))        
+        g_markup.add(button)
+    chosen_f = db_utils.get_faculty_name(flty)
+    bot.send_message(call.from_user.id,'Вы выбрали {}.\nВыберите вашу группу:'.format(chosen_f),reply_markup=g_markup)      
+'''
+@bot.message_handler(commands=['deleteme'])
+def delete_me(message):
+    r = db_utils.delete_user(message.chat.id) 
+    if r:
+        bot.send_message(message.chat.id,'Всего доброго, {}'.format(message.from_user.first_name))
+    
+@bot.message_handler(commands=['расп'])
 def test(message):
-    ts = []
-    data = message.text[6:]      
-    with con:
-        con.row_factory = sq.Row
-        cur = con.cursor()
-        
-        cur.execute('SELECT * FROM classes WHERE c_date=?',(data,))
-        p = cur.fetchall()
-        for row in p:
-            msg = '{0}, {1} пара (неделя {2}):\n{3}\n{4}\n{5}\n'.format(row['c_date'], row['c_time'],row['week'],row['name'],row['prepod'],row['place'])
-            ts.append(msg)           
+    user_id = message.from_user.id
+    date = message.text[6:]      
+    ts = db_utils.get_sch_by_date(user_id,date)
+    if not ts:
+        bot.send_message(message.chat.id,'Я не могу найти ваше расписание на {}.'.format(date))
     for m in ts:            
         bot.send_message(message.chat.id,m)
 
 if __name__=='__main__':
-    bot.polling(none_stop=True)
-    time.sleep(100)
+    bot.polling()
+    print(db_utils.check_users())
